d1653693c5e72b4f6037fd8ed775d30c112ce9d1
Dave <DaveBathnes@users.noreply.github.com>
2019-05-02 08:48:33 +0100

    #9 - Modifications to mobile view. Closes #9.

--- views/vw_mobiles.sql
+++ views/vw_mobiles.sql
@@ -1,16 +1,13 @@
 create view vw_mobiles as
 select distinct
     m.id as id,
-    m.name as mobile_name,
-    m.timetable as mobile_timetable,
-    o.id as organisation_id,
-    o.name as organisation_name,
-    o.timetable as organisation_timetable,
+    m.organisation_id as organisation_id,
+    m.name as name,
+    m.timetable as timetable,
     count(distinct r.id) as number_routes,
     count(distinct s.id) as number_stops
 from mobile m
-join organisation o on m.organisation_id = o.id
 join route r on r.mobile_id = m.id
 join stop s on s.route_id = r.id
-group by m.id, m.name, o.id, o.name, o.timetable
-order by m.id, m.name, o.id, o.name, o.timetable;
+group by m.id, m.organisation_id, m.name, m.timetable
+order by m.organisation_id, m.id, m.name;
