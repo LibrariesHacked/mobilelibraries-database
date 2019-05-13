b45a07f7dbfaaada7515fe1e415ad5407e151257
Dave <DaveBathnes@users.noreply.github.com>
2019-05-13 18:40:16 +0100

    #13 - Completing function for vector tiles. Closes #13.

--- functions/fn_stops_mvt.sql
+++ functions/fn_stops_mvt.sql
@@ -2,14 +2,15 @@ create or replace function fn_stops_mvt(x integer, y integer, z integer) returns
 $$
 declare 
     bbox geometry := fn_bbox(x, y, z);
+	tile bytea;
 begin
-select ST_AsMVT(s, 'stops', 4096, 'geom')
+select st_asmvt(s, 'stop', 4096, 'mvt_geom') into tile
 from (
-    select id, name, community, address, postcode, arrival, departure, timetable, ST_AsMvtGeom(geom, bbox, 4096, 256, true) AS geom
-    from stops
-    where geom && bbox
-    and ST_Intersects(geom, bbox)
+    select id, name, community, address, postcode, arrival, departure, timetable, st_asmvtgeom(st_transform(geom, 3857), bbox, 4096, 256, true) as mvt_geom
+    from stop
+    where st_intersects(st_transform(geom, 3857), bbox)
 ) as s;
+return tile;
 end;
 $$
 language plpgsql;
