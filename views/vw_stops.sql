f2461f9f5cbf0e773fe8377fad1c008c739fcd21
Dave <DaveBathnes@users.noreply.github.com>
2019-07-15 21:01:44 +0100

    #27 - Adding to stops view

--- views/vw_stops.sql
+++ views/vw_stops.sql
@@ -2,8 +2,11 @@ create view vw_stops as
 select distinct
     s.id as id,
     r.id as route_id,
+    r.name as route_name,
     m.id as mobile_id,
+    m.name as mobile_name,
     o.id as organisation_id,
+    o.name as organisation_name,
     s.name as name,
     s.community as community,
     s.address as address,
@@ -11,6 +14,17 @@ select distinct
     s.arrival as arrival,
     s.departure as departure,
     s.timetable as timetable,
+    r.start as route_start,
+    r.end as route_end,
+	to_char(r.start, 'Day') as route_day,
+	r.frequency as route_frequency,
+	to_jsonb(array(select instance::date from (select rrule_event_instances_range(
+		r.start,
+		r.frequency, 
+		(now() at time zone 'Europe/London')::date,
+		coalesce(r.end, now() + interval '1 year'),
+		52
+    ) as instance) as instances)) as dates,
 	st_x(s.geom) as longitude,
 	st_y(s.geom) as latitude
 from stop s
