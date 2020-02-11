254dfc82fa24dae742f22044fb766a065fd51088
Dave <DaveBathnes@users.noreply.github.com>
2020-02-11 21:42:50 +0000

    #48 Fixing bug in route dates

--- functions/fn_update_route_dates.sql
+++ functions/fn_update_route_dates.sql
@@ -4,7 +4,7 @@ declare
     last_updated date := null;
 begin
 
-    -- If we've estimated locations in the last day then don't do it again
+    -- If we've updated route dates in the last day then don't do it again
     select updated::date into last_updated from updates where type = 'route_dates';
     if last_updated is not null and last_updated > (now() at time zone 'Europe/London')::date then
         return;
@@ -29,6 +29,7 @@ begin
     from route r;
 
     truncate route_dates;
+
     insert into route_dates(route_id, route_date)
     select route_id, route_date from temp_route_dates;
 
@@ -38,7 +39,7 @@ begin
     if last_updated is not null then
         update updates set updated = (now() at time zone 'Europe/London') where type = 'route_dates';
     else
-        insert into updates (type, updated) values ('routes_dates', (now() at time zone 'Europe/London'));
+        insert into updates (type, updated) values ('route_dates', (now() at time zone 'Europe/London'));
     end if;
 
     return;
